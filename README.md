5ef711934d204af4383b5ce71d32e27bad378fd3
Joris DANIEL <joris.daniel@gmail.com>
2017-02-07 00:25:26 +0100

    Initial commit 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,136 @@
-# youtube-player-js
-Easy way to load and manage multiple Youtube player with API
+![youtube-player-js](https://img.shields.io/badge/Youtube_Player_JS-v1.0.0-000000.svg?style=flat-square)
+![Youtube](https://img.shields.io/badge/Youtube_API-Player|Iframe-c51109.svg?style=flat-square)
+
+# [Youtube Player JS](http://yoriiis.github.io/youtube-player-js)
+
+Youtube player module allow you to automatically load Youtube API (iframe_api or player_api), parse DOM and start instantiation. Each player is available with a small boilerplate (poster and play button).
+
+You can managed all videos Youtube on your page with a global javascript object `PlayerYT`. It is write in vanillaJS, no needs to import jQuery.
+
+## Installation
+
+Call youtube player module in your HTML before your application and use it.
+
+```html
+<script src="js/youtube-player.js"></script>
+```
+
+## How it work
+
+### HTML structure
+
+Use HTML structure below without change. Replace just `{{idSelector}}` with a unique id and `{{urlVideo}}` with the url of the video. Integration in .player-poster tag can be easily modified.<br />
+_For information, all player must have a unique id._
+
+```html
+<div class="container-player">
+  <div class="player-yt-js">
+    <div id="player-youtube-{{idSelector}}" class="player-js" data-id="{{idVideo}}"></div>
+  </div>
+  <div class="player-poster"></div>
+</div>
+```
+
+### Options
+
+* `api` iframe_api or player_api
+* `autoLoadAPI` automatically load Youtube API
+* `parsePlayer` automatically parse all Youtube players in the page
+* `ignoreSelector` ignore specific player (string selector class or id)
+* `multiplePlaying` disable multiple player Youtube playing in the same time
+* `optionsPlayer` player parameters pass to Youtube API (`playerVars` object)
+
+### Instanciation
+
+```javascript
+var playerYT = new PlayerYT({
+    api: 'iframe_api',
+    autoLoadAPI: true,
+    parsePlayer: true,
+    ignoreSelector: '',
+    multiplePlaying: true,
+    optionsPlayer: {
+        'showinfo': 0,
+        'modestbranding': 0,
+        'autohide': 0,
+        'rel': 0,
+        'wmode': 'transparent'
+    }
+});
+```
+
+### Load Youtube API
+
+If `autoLoadAPI` option is disabled, you must load Youtube API on your side.<br />On `onYouTubeIframeAPIReady` Youtube event, use `playerYT.reParse()` to parse all your Youtube player.
+
+```javascript
+window.onYouTubeIframeAPIReady = function(){
+    playerYT.reParse();
+};
+```
+
+### Parsing DOM
+
+On module player init, with `parsePlayer` enabled, CSS class are added to every Youtube player parsed. You can re-parse the DOM with `playerYT.reParse()`.
+
+```javascript
+playerYT.reParse();
+```
+
+This method accept one parameter, a string with selector class or id to parse specific player on your page.
+
+```javascript
+playerYT.reParse('.player-yt-js');
+```
+
+### Callback functions
+
+There are multiple callback available with module player. If callback exist, default behavior is overrided.
+
+* [`onCallbackYoutubeAPIReady`](#onCallbackYoutubeAPIReady)
+* [`onCallbackPlayerReady`](#onCallbackPlayerYTReady)
+* [`onCallbackPlayerStateChange`](#onCallbackPlayerStateChange)
+* [`onCallbackPlayerClickPoster`](#onCallbackPlayerClickPosterYT)
+
+#### <a name="onCallbackYoutubeAPIReady"></a>On callback youtube API ready
+
+Function called when Youtube API call window.onYouTubeIframeAPIReady
+
+```javascript
+playerYT.onCallbackYoutubeAPIReady = function(){ };
+```
+
+#### <a name="onCallbackPlayerYTReady"></a>On callback player ready
+
+Function called when every player is ready and instanciated. `data` parameter is player instance.
+
+```javascript
+playerYT.onCallbackPlayerReady = function(data){ };
+```
+
+#### <a name="onCallbackPlayerStateChange"></a>On callback player state change
+
+Function called when player status changed. There is a default behavior to show poster when video is ended with `show()` jQuery method. You can change this behavior with this callback function.
+
+```javascript
+playerYT.onCallbackPlayerStateChange = function(state){ };
+```
+
+Here is the different value of `state.data`, more informations on <a href="https://developers.google.com/youtube/iframe_api_reference" title="Youtube API documentation" target="_blank">Youtube API documentation</a>.
+
+| Value        | Status        |
+| ------------ | ------------- |
+| -1           | not started   |
+| 0            | stop          |
+| 1            | playing       |
+| 2            | paused        |
+| 3            | buffering     |
+| 5            | queued        |
+
+#### <a name="onCallbackPlayerClickPosterYT"></a>On callback player poster click
+
+Function called on poster click. `e` parameter is click event, `instancePlayer` is the instance of the player. There is a default behavior to hide poster on click with `hide()` jQuery method and to play video. You can change this behavior with this callback function.
+
+```javascript
+playerYT.onCallbackPlayerClickPoster = function(e, instancePlayer){ };
+```
---
+++ js/youtube-player.js
@@ -0,0 +1,243 @@
+/**
+ *
+ * Module: Youtube player js
+ * @version 1.0.0
+ * @author: Joris DANIEL
+ * @fileoverview: Easy way to load and manage multiple Youtube player with API
+ * Compatibilities : Youtube API (iframe & player)
+ *
+ * Copyright (c) 2017 Joris DANIEL
+ * Licensed under the MIT License: http://www.opensource.org/licenses/mit-license.php
+ *
+ **/
+
+(function(){
+
+    var PlayerYT = function( options ){
+
+        this.youtubeAPIReady = false;
+        this.players = [];
+        this.playerOnPlaying = [];
+
+        var defaultOptions = {
+            api: 'iframe_api',
+            autoLoadAPI: true,
+            parsePlayer: true,
+            ignoreSelector: '',
+            multiplePlaying: true,
+            optionsPlayer: {
+                'showinfo': 0,
+                'modestbranding': 0,
+                'autohide': 0,
+                'rel': 0,
+                'wmode': 'transparent'
+            }
+        };
+
+        this.options = utils.extend({}, defaultOptions, options);
+
+        this.reParse = function(selector){
+            this.parsePlayer(selector);
+        }
+
+        if( this.options.autoLoadAPI ){
+            this.loadYoutubeAPI();
+        }
+
+    };
+
+    PlayerYT.prototype.loadYoutubeAPI = function(){
+
+        var _this = this;
+
+        window.onYouTubeIframeAPIReady = function(){
+            _this.onYouTubeIframeAPIReadyCallback();
+        }
+
+        if( this.options.autoLoadAPI ){
+            var urlAPI = 'https://youtube.com/' + _this.options.api;
+            if( this.autocomplete ){
+                urlAPI += '&libraries=places';
+            }
+            var tag = document.createElement('script');
+            tag.async = true;
+            tag.type = 'text/javascript';
+            tag.src = urlAPI;
+            document.getElementsByTagName('body')[0].appendChild(tag);
+        }
+
+
+    };
+
+    PlayerYT.prototype.onYouTubeIframeAPIReadyCallback = function(){
+
+        this.youtubeAPIReady = true;
+
+        if (typeof this.onCallbackYoutubeAPIReady === 'function') {
+            this.onCallbackYoutubeAPIReady();
+        }
+
+        if( this.options.parsePlayer ){
+            this.parsePlayer();
+        }
+
+    };
+
+    PlayerYT.prototype.parsePlayer = function(selector){
+
+        var _this = this,
+            instancePlayer = null;
+
+        //Parse specific player
+        if( typeof selector !== 'undefined' ){
+            var selector = document.querySelectorAll(selector);
+        }else{
+            //Parse all selector by default
+            var selectorString = '.player-yt-js:not(.parsed)';
+            if( this.options.ignoreSelector !== '' ){
+                var selectorIgnoredArray = this.options.ignoreSelector.split(',');
+                for( var j = 0, lengthIgnoreSelector = selectorIgnoredArray.length; j < lengthIgnoreSelector; j++ ){
+                    selectorString += ':not(' + selectorIgnoredArray[j] + ')';
+                }
+            }
+            var selector = document.querySelectorAll(selectorString);
+        }
+
+        for( var i = 0, lengthSelector = selector.length; i < lengthSelector; i++ ){
+
+            var element = selector[i],
+                selectorId = element.querySelector('.player-js').getAttribute('id'),
+                videoId = element.querySelector('.player-js').getAttribute('data-id'),
+                playerPoster = element.parentNode.querySelector('.player-poster');
+
+            utils.addClass('parsed', element);
+
+            //Init Youtube player
+            instancePlayer = new YT.Player(selectorId, {
+                videoId: videoId,
+                height: '100%',
+                width: '100%',
+                playerVars: this.options.optionsPlayer,
+                events: {
+                    'onReady': function(data){
+                        if (typeof _this.onCallbackPlayerReady === 'function') {
+                            _this.onCallbackPlayerReady(data);
+                        }
+                    },
+                    'onStateChange': function(state){
+
+                        var currentParentIframe = state.target.getIframe().parentNode.parentNode,
+                            selectorId = currentParentIframe.querySelector('.player-js').getAttribute('id'),
+                            playerPoster = currentParentIframe.querySelector('.player-poster');
+
+                        //All player playing are saved in array
+                        if( state.data === 1 ){
+                            _this.playerOnPlaying.push(selectorId);
+                        }else{
+                            _this.updateArrayPlayersPlaying(selectorId);
+                        }
+
+                        if(!_this.options.multiplePlaying && state.data === 1){
+                            _this.pauseOtherVideo(selectorId);
+                        }
+
+                        if (typeof _this.onCallbackPlayerStateChange === 'function') {
+                            _this.onCallbackPlayerStateChange(state);
+                        }else{
+                            //On video ended, show poster video
+                            if( state.data === 0 ){
+                                playerPoster.style.display = 'block';
+                            }
+                        }
+
+                    }
+                }
+            });
+
+            element.setAttribute('data-yt-key', _this.players.length);
+            this.players.push(instancePlayer);
+
+            //Start video on poster click, and hide it
+            playerPoster.addEventListener('click', function(e){
+
+                var instancePlayer = _this.players[e.target.parentNode.querySelector('.player-yt-js').getAttribute('data-yt-key')];
+
+                e.preventDefault();
+
+                if (typeof _this.onCallbackPlayerClickPoster === 'function') {
+                    _this.onCallbackPlayerClickPoster(e, instancePlayer);
+                }else{
+                    instancePlayer.playVideo();
+                    e.target.style.display = 'none';
+                }
+
+            });
+
+        }
+
+    };
+
+
+    //If player is paused or ended, removed from array
+    PlayerYT.prototype.updateArrayPlayersPlaying = function(currentSelectorId){
+
+        for( var i = 0, lengthPlayerPlaying = this.playerOnPlaying.length; i < lengthPlayerPlaying; i++ ){
+            if( this.playerOnPlaying[i] === currentSelectorId ){
+                this.playerOnPlaying.splice(i, 1);
+            }
+        }
+
+    };
+
+    //If multiplePlaying option is disabled, pause all other video before play current video
+    PlayerYT.prototype.pauseOtherVideo = function(currentSelectorId){
+
+        if( this.playerOnPlaying.length > 1 ){
+            for( var i = 0, lengthPlayerPlaying = this.playerOnPlaying.length; i < lengthPlayerPlaying; i++ ){
+                //Prevent pause current player
+                if( this.playerOnPlaying[i] !== currentSelectorId ){
+                    var currentPlayerKey = document.querySelector('#' + this.playerOnPlaying[i]).parentNode.getAttribute('data-yt-key')
+                    var instancePlayer = this.players[currentPlayerKey];
+                    instancePlayer.pauseVideo()
+                }
+            }
+        }
+
+    };
+
+    var utils = {
+        addClass: function(className, selector) {
+            if (selector.classList){
+                selector.classList.add(className);
+            }else{
+                selector.className += ' ' + className;
+            }
+        },
+        extend: function(out) {
+            out = out || {};
+
+            for (var i = 1; i < arguments.length; i++) {
+                var obj = arguments[i];
+
+                if (!obj){
+                    continue;
+                }
+
+                for (var key in obj) {
+                    if (obj.hasOwnProperty(key)) {
+                        if (typeof obj[key] === 'object'){
+                            out[key] = this.extend(out[key], obj[key]);
+                        }else{
+                            out[key] = obj[key];
+                        }
+                    }
+                }
+            }
+
+            return out;
+        }
+    };
+
+    window.PlayerYT = PlayerYT;
+
+})();
